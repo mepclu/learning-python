28ee30781bb5986261a21868a908ba52816b4370
mepclu <mary.e.peterson@gmail.com>
2014-01-15 14:19:48 -0500

    assignment3.py

---
+++ assignment3.py
@@ -0,0 +1,18 @@
+
+year_married=2010
+current_year=2014
+x=current_year - year_married
+cats = 2
+dog = 1
+animals = cats + dog
+mult = animals * cats
+div = dog/cats
+
+
+print "I got married in", year_married,"."
+print "In May I will have been married for", x, "years"
+print "I have", animals, "pets at my house."
+print "I would like to have %d pets." % mult
+print "My husband would like to have", div, "pets."
+
+print 5**18
