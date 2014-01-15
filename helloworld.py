019a51fc6129b706cc8731535021357e4e21b4cc
mepclu <mary.e.peterson@gmail.com>
2014-01-15 13:29:04 -0500

    helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#learning python commands
+
+print "Hello word"
+
+print "my name is mary"
+
+print "let's count things"
+
+print "boys", 25 + 30 / 6
+print "girls", 100 - 25 * 3 % 4
+
+print "Let's count the class."
+
+print 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 +6  
